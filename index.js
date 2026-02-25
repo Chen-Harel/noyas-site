ca8cf0d7faa3f7ac643e3399de3a804337c52d94
Chen Harel <chen.harel123@gmail.com>
2026-02-25 21:39:17 +0200

    create full accordion + mobile friendly

--- index.js
+++ index.js
@@ -2,16 +2,13 @@ const acc = document.getElementsByClassName("accordion-header");
 
 for (let i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function () {
-        // Toggle the "active" class to style the button
+        // This line is key! It toggles the class that rotates the arrow
         this.classList.toggle("active");
 
-        // Select the next element (the panel)
         const panel = this.nextElementSibling;
-
         if (panel.style.maxHeight) {
             panel.style.maxHeight = null;
         } else {
-            // scrollHeight gives us the height of the content in pixels
             panel.style.maxHeight = panel.scrollHeight + "px";
         }
     });
